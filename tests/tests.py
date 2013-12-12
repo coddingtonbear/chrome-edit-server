de85cf3cac62ca84a845f6cdf2efcbc37d1deb91
Alexey Kotlyarov <a@koterpillar.com>
2013-12-12 20:33:03 +1100

    Initial test structure

---
+++ tests/tests.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class TestInteraction(unittest.TestCase):
+    def setUp(self):
+        raise Exception("Not implemented")
+
+    def test_edit_file(self):
+        raise Exception("Not implemented")
