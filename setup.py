05c767d26c657c5f667922b4877fbe01e9949670
Adam Coddington <adam.coddington@coxinc.com>
2014-08-22 20:23:37 -0700

    Updating setup.py to mark myself as the maintainer; setting package name.

--- setup.py
+++ setup.py
@@ -10,20 +10,20 @@ with open(
     VERSION = version_file.read().strip()
 
 setup(
-    name='edit-server',
+    name='chrome-edit-server',
     version=VERSION,
-    url='https://github.com/gfxmonk/edit-server',
+    url='https://github.com/coddingtonbear/chrome-edit-server',
     description=(
         'An edit server compatible with TextAid and '
         '"Edit with Emacs" chrome extensions"'
     ),
-    author='Tim Cuthbertson',
-    author_email='tim@gfxmonk.net',
+    author='Tim Cuthbertson, Adam Coddington',
+    author_email='me@adamcoddington.net',
     packages=find_packages(),
     include_package_data=True,
     entry_points={
         'console_scripts': [
-            'edit_server = edit_server:main'
+            'chrome_edit_server = edit_server:main'
         ],
     }
 )
